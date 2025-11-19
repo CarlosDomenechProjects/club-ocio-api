c0db57632df585962882ddcd5df0321e8ab2bbd8
Dom_64329455 <carlos.domenech@correcta.es>
2025-11-19 13:06:15 +0100

    create modules for entities (provisional entities)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './modules/users/users.module';
+import { BusinessModule } from './modules/business/business.module';
+import { EventsModule } from './modules/events/events.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
 
 @Module({
   imports: [
@@ -8,6 +12,10 @@ import { DatabaseModule } from './database/database.module';
       isGlobal: true,
     }),
     DatabaseModule,
+    UsersModule,
+    BusinessModule,
+    EventsModule,
+    BookingsModule,
   ],
   controllers: [],
   providers: [],
